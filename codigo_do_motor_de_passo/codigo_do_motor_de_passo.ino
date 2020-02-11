61e9772a08545d159620013c3d9c553ab3d60f50
delclecianojunior <delclecianobj@hotmail.com>
2020-02-11 13:08:23 -0300

    Criando funçao de abrir e fechar de motor de passo

---
+++ codigo_do_motor_de_passo/codigo_do_motor_de_passo.ino
@@ -0,0 +1,50 @@
+int motorPin1 = 8;
+int motorPin2 = 9;
+int motorPin3 = 10;
+int motorPin4 = 11;
+int delayTime = 10;
+
+void setup() {
+ InicializarPinosMotor();
+}
+
+void loop() { 
+ GirarParaDireita(2);
+ //GirarParaEsquera(5);
+}
+
+void InicializarPinosMotor(){
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+}
+
+void GirarParaDireita(int delayTime){
+  EnergizarPinos(HIGH,LOW,LOW, LOW);
+  delay(delayTime);
+  EnergizarPinos(LOW,HIGH,LOW, LOW);
+  delay(delayTime);
+  EnergizarPinos(LOW,LOW,HIGH, LOW);
+  delay(delayTime);
+  EnergizarPinos(LOW,LOW,LOW, HIGH);
+  delay(delayTime);
+}
+
+void GirarParaEsquera(int delayTime){
+  EnergizarPinos(LOW,LOW,LOW, HIGH);
+  delay(delayTime);
+  EnergizarPinos(LOW,LOW,HIGH,LOW);
+  delay(delayTime);
+  EnergizarPinos(LOW,HIGH,LOW,LOW);
+  delay(delayTime);
+  EnergizarPinos(HIGH,LOW,LOW,LOW);
+  delay(delayTime); 
+}
+
+void EnergizarPinos(int statusPino1, int statusPino2, int statusPino3, int statusPino4){
+  digitalWrite(motorPin1, statusPino1);
+  digitalWrite(motorPin2, statusPino2);
+  digitalWrite(motorPin3, statusPino3);
+  digitalWrite(motorPin4, statusPino4);
+}
